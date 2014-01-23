70de55d055f7dc5c78cba7cf6cf95128d1b0897e
Matt Hartley <mbhartley1490@gmail.com>
2014-01-22 23:09:00 -0500

    homework complete

---
+++ README.md
@@ -0,0 +1,28 @@
+EXAMPLE 1 is rickyBobby. It contains some personal information about rickyBobby that you can 
+call up by prompting rickyBobby to:
+
+first: rickyBobby.giveAddress
+second: rickyBobby.giveFullAddress
+third: rickyBobby.quoteFavoritePrayer
+
+rickyBobby will return his best answers to these requests!
+
+
+EXAMPLE 2 is heyEinstein. Einstein will tell you the number of hydrogen atoms per water molecules when you prompt 
+
+heyEinstein.countHydrogen
+
+I'm not completely pleased with this one when its run multiple times b/c Einstein's variables
+are limited by the simplicity of my formula!
+
+
+EXAMPLE 3 is masonEats. It tells Mason how much he will weigh after eating donuts vs. after
+eating kale. Basically, he gains 5lbs when he eats donuts, and loses 2lbs when he eats kale. So, you can run it every time he eats one or the other, and it will keep a live tally of his current weight. Try:
+
+masonEats.donuts
+or
+masonEats.kale
+
+
+
+
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,53 @@
-console.log('\'Allo \'Allo!');
+ //EXAMPLE 1//
+
+ rickyBobby = {firstName: 'Ricky', lastName: 'Bobby', address: '2153 Fast Lane ',
+			city: 'Talledega', state: 'Alabama ', zipCode: '53241', favoriteColor: 'red',
+			favoritePrayer: 'Dear baby Jesus'}
+
+			rickyBobby.quoteFavoritePrayer = function(){
+				return "My favorite prayer begins, " + this.favoritePrayer
+			}
+
+			rickyBobby.giveAddress = function (){
+				return "My address is " + this.address
+			}
+
+			rickyBobby.giveFullAddress = function (){
+				return ("Oh, sorry! My full address is " + this.address + "in the state of " + 
+					this.state + "and my zip is " + this.zipCode)
+			}
+
+//EXAMPLE 2//
+
+ heyEinstein = {molecule: 2}
+
+			heyEinstein.countHydrogen = function (){
+				this.molecule = this.molecule * 2;
+				this.hydrogen = this.molecule * 2;
+
+				return (this.molecule + " water molecules contain " + this.hydrogen + 
+				" hydrogen atoms.")
+			}
+
+//EXAMPLE 3//
+
+ masonEats = {weight: 150, donuts: 5, kale: -2}
+
+ 			masonEats.donuts = function (){
+ 				this.weight = this.weight + 5;
+
+ 				return ("Mason now weighs " + this.weight + " pounds!");
+ 			}
+ 			masonEats.kale = function (){
+ 				this.weight = this.weight -2;
+
+ 				return ("Mason now weighs " + this.weight + " pounds!");
+ 			}
+
+
+
+
+
+
+
+
